af19b885903bfee0943f323d38f48637dbe12299
Alexandra Koval <alexandra.koval101@gmail.com>
2024-07-31 13:08:03 +0300

    Initial Commit

---
+++ Task Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task Movies
+//
+//  Created by Alexandra on 31.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
